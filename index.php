69a649e56e4fe4677935f502fe924fb718d5eab9
97mams <97mams@gmail.com>
2024-05-11 10:26:46 +0300

    update variable

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ $word = readline("entrez un mot : ");
 
 
 if (!(int)$word) {
-    $cryptword = new App\Controller\CryptController($word);
-    echo $cryptword->getWordReversed();
+    $encryptword = new App\Controller\CryptController($word);
+    echo $encryptword->getWordReversed();
 } else {
     echo "essayez avec une chaîne de caractère";
 }
